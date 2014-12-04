f4fc3ce16cc4e45c9d4a2a22466015eb0aedf18a
lulumarshall <lulumarshall@gmail.com>
2014-12-04 17:22:16 +0000

    added pin point for address and current location

--- app/assets/javascripts/locations.js
+++ app/assets/javascripts/locations.js
@@ -1,23 +1,53 @@
-mapAjax = {}
-  var map;
-mapAjax.initialize = function(location){
-  console.log(location);
-  var currentLocation = new google.maps.LatLng(location.coords.latitude, location.coords.longitude)
+mapAjax = {
+  map: null
+}
+  var geocoder;
+
+
+mapAjax.drawMap = function(location){
   var mapOptions = {
       zoom:16,
-      center: currentLocation,
+      center: location,
       
     }
-  map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+  mapAjax.map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
   var marker = new google.maps.Marker({
-    position: currentLocation,
-    map: map
+    position: location,
+    map: mapAjax.map
   });
 }
+
+mapAjax.geocodeAddress = function(address){
+  geocoder = new google.maps.Geocoder();
+  geocoder.geocode( { 'address': address}, function(results, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
+        console.log("results", results)
+        mapAjax.initialize(results[0].geometry.location);
+        
+      } else {
+        alert('Geocode was not successful for the following reason: ' + status);
+      }
+    });
+}
+
+mapAjax.getLatLng = function(location){
+  console.log(location);
+};
 
 $(document).ready(function(){
   console.log('dom loaded')
   $('.map').on('click', function(event){
-    navigator.geolocation.getCurrentPosition(mapAjax.initialize)
+    navigator.geolocation.getCurrentPosition(function(location){
+      console.log()
+      var currentLocation = new google.maps.LatLng(location.coords.latitude, location.coords.longitude)
+      mapAjax.drawMap(currentLocation)
+    })
+  });
+  $('#address').on('keypress', function(){
+    if(event.which== '13'){
+      var location = $(this).val()
+      
+      mapAjax.geocodeAddress(location)
+    };
   });
 })
