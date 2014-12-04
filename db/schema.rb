8fc72b3b4e36275ea215dc40865e903a6ae0bc4e
lulumarshall <lulumarshall@gmail.com>
2014-12-04 09:51:32 +0000

    added locations associations of user_id

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20141203213923) do
+ActiveRecord::Schema.define(:version => 20141204094658) do
 
   create_table "cycles", :force => true do |t|
     t.string   "user_id"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(:version => 20141203213923) do
     t.string   "title"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.integer  "user_id"
   end
 
   create_table "messages", :force => true do |t|
