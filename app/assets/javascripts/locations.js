531d88f114886c0c627d12915afbe6ff49b8eb47
lulumarshall <lulumarshall@gmail.com>
2014-12-05 10:31:36 +0000

    added ajax request to be sent to the locations index method

--- app/assets/javascripts/locations.js
+++ app/assets/javascripts/locations.js
@@ -5,11 +5,11 @@ mapAjax = {
 
 
 mapAjax.drawMap = function(location){
+  //var currentLocation = new google.maps.LatLng(latitude, longitude)
   var mapOptions = {
-      zoom:16,
-      center: location,
-      
-    }
+    zoom:16,
+    center: location,  
+  }
   mapAjax.map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
   var marker = new google.maps.Marker({
     position: location,
@@ -20,15 +20,30 @@ mapAjax.drawMap = function(location){
 mapAjax.geocodeAddress = function(address){
   geocoder = new google.maps.Geocoder();
   geocoder.geocode( { 'address': address}, function(results, status) {
-      if (status == google.maps.GeocoderStatus.OK) {
-        console.log("results", results)
-        mapAjax.initialize(results[0].geometry.location);
-        
-      } else {
-        alert('Geocode was not successful for the following reason: ' + status);
-      }
-    });
+    if (status == google.maps.GeocoderStatus.OK) {
+      console.log("results", results)
+      mapAjax.drawMap(results[0].geometry.location);
+      mapAjax.findRiders(results[0].geometry.location)
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }
+  });
 }
+
+mapAjax.findRiders = function(location){
+  $.ajax({
+    url: '/locations', 
+    type: 'GET', 
+    data: {location: {
+      lat: location.k,
+      lng: location.B
+    }},
+    dataType: 'json' 
+  }).success(function(data){
+    debugger;
+  })
+}
+
 
 mapAjax.getLatLng = function(location){
   console.log(location);
@@ -43,10 +58,10 @@ $(document).ready(function(){
       mapAjax.drawMap(currentLocation)
     })
   });
+  
   $('#address').on('keypress', function(){
     if(event.which== '13'){
       var location = $(this).val()
-      
       mapAjax.geocodeAddress(location)
     };
   });
